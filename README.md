3bed5a3c22faf167d8fa29f6c3f8eb206b470a9f
Antony <antony.suresh@gmail.com>
2022-06-21 12:48:20 +0530

    added files for basic greetings service in express

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
-# backend-api for simple api
+# backend-api for simple backend api
+
 yarn Commands :
-   - build
-   - dev
-   - start
-   - test:unit
-   
+
+- build
+- dev
+- start
+- test:unit
+
 Tech Stack :
-  - Express
-  - OpenAPI/Swagger
-  - Jest
-  - supertest
-  - Yarn
+
+- Express middleware
+- OpenAPI/Swagger
+- Jest for unit testing
+- supertest for api testing
+- morgan and winston for logging
 
 Prerequisites:
-  - Node 16.10.0 and Yarn installed
+
+- Node 16.10.0 and Yarn installed
+
+Referred From :
+
+- https://losikov.medium.com/backend-api-server-development-with-node-js-from-scratch-to-production-fe3d3b860003
---
+++ src/utils/logger.ts
@@ -0,0 +1,37 @@
+import config from '@api/config'
+import winston from 'winston'
+
+// npm debug levels (winston default):
+// {
+//   error: 0,
+//   warn: 1,
+//   info: 2,
+//   http: 3
+//   verbose: 4,
+//   debug: 5,
+//   silly: 6
+// }
+
+const prettyJson = winston.format.printf(info => {
+  if (info.message.constructor === Object) {
+    info.message = JSON.stringify(info.message, null, 4)
+  }
+  return `${info.timestamp} ${info.label || '-'} ${info.level}: ${info.message}`
+})
+
+const logger = winston.createLogger({
+  level: config.loggerLevel === 'silent' ? undefined : config.loggerLevel,
+  silent: config.loggerLevel === 'silent',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.prettyPrint(),
+    winston.format.splat(),
+    winston.format.simple(),
+    winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss.SSS'}),
+    prettyJson
+  ),
+  defaultMeta: {service: 'backend-api'},
+  transports: [new winston.transports.Console({})]
+})
+
+export default logger
